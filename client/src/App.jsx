78ad582c350710b60ab844aac18e1688c9898589
vitwits <vitwits@gmail.com>
2025-05-30 21:55:11 +0200

    adding navbar and header, added blog and home pages, adding base routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+    </>
+  );
+};
+
+export default Home;
