5867bfd6eda23cb7f68ba172e080635ffc63ffbd
vitwits <vitwits@gmail.com>
2025-06-02 21:25:44 +0200

    updated login page

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
   };
@@ -16,10 +18,15 @@ const Login = () => {
               Enter your credentials to access the admin panel
             </p>
           </div>
-          <form onSubmit={handleSubmit}>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-6 w-full sm:max-w-md text-gray-600"
+          >
             <div className="flex flex-col">
               <label>Email</label>
               <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 type="email"
                 required
                 placeholder="your email id"
@@ -29,13 +36,21 @@ const Login = () => {
             <div className="flex flex-col">
               <label>Password</label>
               <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
                 type="password"
                 required
                 placeholder="your password"
                 className="border-b-2 border-gray-300 p-2 outline-none mb-6"
               />
             </div>
-            <button type="submit" className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all"> Login </button>
+            <button
+              type="submit"
+              className="w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all"
+            >
+              {" "}
+              Login{" "}
+            </button>
           </form>
         </div>
       </div>
