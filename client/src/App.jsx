f48a11d6a408e3e9733d42a41ef6893176364ac0
vitwits <vitwits@gmail.com>
2025-06-15 22:32:11 +0200

    added context and toasts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,20 +9,22 @@ import Comments from "./pages/admin/Comments";
 import Dashboard from "./pages/admin/Dashboard";
 import Login from "./components/admin/Login";
 import "quill/dist/quill.snow.css";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blog/:id" element={<Blog />} />
-        <Route path="/admin" element={true ? <Layout /> : <Login />}>
-          <Route index element={<Dashboard />} />
-          <Route path="addBlog" element={<AddBlog />} />
-          <Route path="listBlog" element={<ListBlog />} />
-          <Route path="comments" element={<Comments />} />
-        </Route>
-      </Routes>
+      <Toaster />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path="/admin" element={true ? <Layout /> : <Login />}>
+            <Route index element={<Dashboard />} />
+            <Route path="addBlog" element={<AddBlog />} />
+            <Route path="listBlog" element={<ListBlog />} />
+            <Route path="comments" element={<Comments />} />
+          </Route>
+        </Routes>
     </div>
   );
 };
